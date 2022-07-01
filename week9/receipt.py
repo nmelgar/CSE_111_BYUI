bf3e69b0d7087d46d6ecfddc46321d41f0a9eca8
nmelgar <nmelgar16013@gmail.com>
2022-06-30 23:02:07 -0500

    Working with CSV files.

---
+++ week9/receipt.py
@@ -0,0 +1,64 @@
+from asyncore import read
+import csv
+
+
+def main():
+    order1 = "D150"
+    list1 = read_dict("week9/products.csv", 0)
+    order = list1[order1]
+    order2 = order[0]
+    # print(list1)
+    print(order2)
+
+
+def read_dict(filename, key_column_index):
+    """Read the contents of a CSV file into a compound
+    dictionary and return the dictionary.
+
+    Parameters
+        filename: the name of the CSV file to read.
+        key_column_index: the index of the column
+            to use as the keys in the dictionary.
+    Return: a compound dictionary that contains
+        the contents of the CSV file.
+    """
+    # Create an empty dictionary that will
+    # store the data from the CSV file.
+    dictionary = {}
+
+    # Open the CSV file for reading and store a reference
+    # to the opened file in a variable named csv_file.
+    with open(filename, "rt") as csv_file:
+
+        # Use the csv module to create a reader object
+        # that will read from the opened CSV file.
+        reader = csv.reader(csv_file)
+
+        # The first row of the CSV file contains column
+        # headings and not data, so this statement skips
+        # the first row of the CSV file.
+        next(reader)
+
+        # Read the rows in the CSV file one row at a time.
+        # The reader object returns each row as a list.
+        for row_list in reader:
+
+            # From the current row, retrieve the data
+            # from the column that contains the key.
+            # This will use the column that will be used as key
+            key = row_list[key_column_index]
+
+            # Store the data from the current row
+            # into the dictionary.
+            dictionary[key] = row_list
+
+    # Return the dictionary.
+    return dictionary
+
+
+# If this file was executed like this:
+# > python teach_solution.py
+# then call the main function. However, if this file
+# was simply imported, then skip the call to main.
+if __name__ == "__main__":
+    main()
