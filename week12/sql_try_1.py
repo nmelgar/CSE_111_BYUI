e85239c9ec229c25c517538c059f8ef27007442e
nmelgar <nmelgar16013@gmail.com>
2022-07-15 16:26:03 -0500

    Adding tables, inserting rows.

--- week12/sql_try_1.py
+++ week12/sql_try_1.py
@@ -38,10 +38,27 @@ mycursor = mydb.cursor()
 
 # -----> STEP 5
 # check if table exists
-mycursor.execute("SHOW TABLES")
+# mycursor.execute("SHOW TABLES")
 
-for x in mycursor:
-    print(x)
+# for x in mycursor:
+#     print(x)
+
+# -----> STEP 6
+# insert into table using INSERT INTO statement
+# name_to_insert = "John"
+# address_to_insert = "Highway 21"
+# sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+
+## val = ("John", "Highway 21")
+# val = (name_to_insert, address_to_insert)
+
+# mycursor.execute(sql, val)
+
+# Important!: Notice the statement: mydb.commit(). It is
+# required to make the changes, otherwise no changes are made to the table.
+# mydb.commit()
+
+# -----> STEP 7
 
 
 # -------------------------------------------
@@ -51,5 +68,30 @@ for x in mycursor:
 
 # -----> NO NUMBER STEP
 # alter existing table to add PRIMARY KEY
-mycursor.execute(
-    "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+# mycursor.execute(
+#     "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+
+# -----> NO NUMBER STEP
+# insert multiple rows
+# sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+# val = [
+#   ('Peter', 'Lowstreet 4'),
+#   ('Amy', 'Apple st 652'),
+#   ('Hannah', 'Mountain 21'),
+#   ('Michael', 'Valley 345'),
+#   ('Sandy', 'Ocean blvd 2'),
+#   ('Betty', 'Green Grass 1'),
+#   ('Richard', 'Sky st 331'),
+#   ('Susan', 'One way 98'),
+#   ('Vicky', 'Yellow Garden 2'),
+#   ('Ben', 'Park Lane 38'),
+#   ('William', 'Central st 954'),
+#   ('Chuck', 'Main Road 989'),
+#   ('Viola', 'Sideway 1633')
+# ]
+
+# mycursor.executemany(sql, val)
+
+# mydb.commit()
+
+# print(mycursor.rowcount, "was inserted.")
