28667ff892b2969da193899c73b92de44bb3ddb5
nmelgar <nmelgar16013@gmail.com>
2022-07-19 19:22:37 -0500

    You can view tasks, add and delete tasks.

--- week12/todo_list_1.py
+++ week12/todo_list_1.py
@@ -1,3 +1,4 @@
+import imp
 from multiprocessing.sharedctypes import Value
 import mysql.connector
 
@@ -19,31 +20,71 @@ mycursor = mydb.cursor()
 
 
 def main():
+    run = True
     print("|----------------------------------------------|")
     print("|------------------TO-DO List------------------|")
     print("|----------------------------------------------|")
     print("\n")
-    displayMenu()
-    user_choice = int(input("What you want to do?"))
-    if user_choice == 1:
-        addTask()
+
+    while run:
+        print("\n|----------------Current tasks-----------------|")
+        print("|--ID |  Task |  Importance -------------------|")
+        print("|----   -----   -------------------------------|")
+        display_tasks()
+        print("|----------------------------------------------|")
+        display_menu()
+        user_choice = int(input("What you want to do? (1, 2 or 3) "))
+        if user_choice == 1:
+            add_task()
+        elif user_choice == 2:
+            delete_task()
+        elif user_choice == 3:
+            run = False
 
 
-def displayMenu():
+def display_tasks():
+    """
+    This function will make a loop to the elements in the db
+    to display all the tasks that need to be performed.
+    Parameters: It will take no parameters.
+    """
+    mycursor.execute("SELECT * FROM tasks")
+
+    myresult = mycursor.fetchall()
+
+    for row in myresult:
+        print(f"|---{row[0]} | {row[1]} | {row[2]}")
+
+
+def display_menu():
+    """This function will display the elements of the menu
+    so the user can choose what action to perform.
+    Parameters: It will take no parameters.
+    """
     menu_items = ["Add Task", "Delete Task", "Exit"]
     counter = 1
+    print("\n")
     for item in menu_items:
 
         print(f"{counter}. {item}")
         counter += 1
 
 
-def addTask():
+def add_task():
+    """This function will add a tasks and its importance to
+    the db
+    User will have to enter the taks to perform and
+    its importance as input
+    Parameters: It take no parameters.
+    """
     print("\n")
     new_task = input("What are you planning to do? ")
-    importance_text = ("Level of importance?",
-                       "H=High | M=Medium | L=Low")
+    importance_text = ("\nH=High | M=Medium | L=Low\nLevel of importance? ")
     importance_level = input(importance_text)
+    importance_level = importance_level.upper()
+
+    assert importance_level == "H" or importance_level == "M" or importance_level == "L", "Please enter a valid importance level"
+
     # insert into table using INSERT INTO statement
 
     sql = "INSERT INTO tasks (task, importance) VALUES (%s, %s)"
@@ -54,8 +95,22 @@ def addTask():
 
     mydb.commit()
 
-# def deleteTask():
-#     DELETE FROM `tasks` WHERE `tasks`.`id` = 6;
+
+def delete_task():
+    """ This function will delete a tasks and everything about it 
+    from the db
+    User will have to enter the ID number of the task you want to delete.
+    Parameters: It take no parameters.
+    """
+    user_choice = input("Enter the ID of the task you want to delete: ")
+
+    sql = "DELETE FROM tasks WHERE id = %s"
+
+    task_id = (user_choice, )
+
+    mycursor.execute(sql, task_id)
+
+    mydb.commit()
 
 
 if __name__ == "__main__":
